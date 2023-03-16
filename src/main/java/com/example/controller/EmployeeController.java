a49a35340bdeba9b0f493da92aa6a2400128e551
cindy-hj <hj0304hj@gmail.com>
2023-03-16 17:40:16 +0900

    2023 03 16 17:40
    업데이트 추가

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -2,7 +2,10 @@ package com.example.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +32,16 @@ public class EmployeeController {
 	@GetMapping("/employee") // read
 	public List<Employee> findAll() {
 		return employeeService.findAll();
+	}
+	
+	@PatchMapping("/employee") // update
+	public Employee update(@RequestBody Employee emp) {
+		return employeeService.update(emp);
+	}
+	
+	@DeleteMapping("/employee/{id}")
+	public Employee delete(@PathVariable Long id) {
+		return employeeService.delete(id);
 	}
 	
 }
